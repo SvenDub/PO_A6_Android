76f03c62f449b0b5d749ae3bf6e536be1a6ea821
SvenDub <sven.dubbeld1@gmail.com>
2013-12-12 22:25:53 +0100

    ActionBarSherlock
    Maak gebruik van ABS voor compatibiliteit met pre-Honeycomb apparaten.

--- src/nl/rgomiddelharnis/a6/po/MainActivity.java
+++ src/nl/rgomiddelharnis/a6/po/MainActivity.java
@@ -1,22 +1,31 @@
 
 package nl.rgomiddelharnis.a6.po;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class MainActivity extends SherlockFragmentActivity {
+    
+    ActionBar mActionBar;
+    public static final boolean LOCAL_LOGD = true;
+    public static final boolean LOCAL_LOGV = true;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        mActionBar = getSupportActionBar();
+        
         setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getSupportMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
