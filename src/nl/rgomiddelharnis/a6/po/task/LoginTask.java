691e8a4858189a14cbeb023069a041a3c678402c
Sven Dubbeld <sven.dubbeld1@gmail.com>
2014-01-17 10:13:39 +0100

    LoginTask Stub

---
+++ src/nl/rgomiddelharnis/a6/po/task/LoginTask.java
@@ -0,0 +1,19 @@
+
+package nl.rgomiddelharnis.a6.po.task;
+
+import android.os.AsyncTask;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class LoginTask extends AsyncTask<List<NameValuePair>, Void, JSONObject> {
+
+    @Override
+    protected JSONObject doInBackground(List<NameValuePair>... params) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
