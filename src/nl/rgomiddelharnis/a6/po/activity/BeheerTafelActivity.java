e7575d26059ef8dddc0fec7edc5e1f7b243252c6
Sven Dubbeld <sven.dubbeld1@gmail.com>
2014-03-21 09:50:06 +0100

    Voeg bestelling toe (1/4)

--- src/nl/rgomiddelharnis/a6/po/activity/BeheerTafelActivity.java
+++ src/nl/rgomiddelharnis/a6/po/activity/BeheerTafelActivity.java
@@ -218,7 +218,7 @@ public class BeheerTafelActivity extends ProgressFragmentActivity implements Tab
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     switch (which) {
-                                        case 0: // TODO: Betalen
+                                        case 0: // Betalen
                                             betaalDialog();
                                             break;
                                         case 1: // TODO: Aanpassen
