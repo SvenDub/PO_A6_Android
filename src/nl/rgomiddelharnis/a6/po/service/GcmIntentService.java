2a4e5a9246f92ad6ad4baaa73cf633d433d059d7
Sven Dubbeld <sven.dubbeld1@gmail.com>
2014-03-24 22:08:45 +0100

    Voeg basis voor Google Play Services en Google Cloud Messaging toe

---
+++ src/nl/rgomiddelharnis/a6/po/service/GcmIntentService.java
@@ -0,0 +1,18 @@
+package nl.rgomiddelharnis.a6.po.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class GcmIntentService extends IntentService {
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
+    
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
